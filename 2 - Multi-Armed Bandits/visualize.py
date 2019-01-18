c398bc3d92033f2f07e2ac316bd718e368c18935
Braden Hoagland <bhoggland@gmail.com>
2019-01-17 23:05:33 -0500

    fixes window bug with visualizer

--- 2 - Multi-Armed Bandits/visualize.py
+++ 2 - Multi-Armed Bandits/visualize.py
@@ -7,27 +7,28 @@ win = None
 def plot(step, reward, title, data, new):
     global win
 
-    if win is None:
-        win = viz.line(
-            X=np.array([step]),
-            Y=np.array([reward]),
-            win=title,
-            name=data,
-            opts=dict(
-                title=title
+    if new:
+        if win != title:
+            win = viz.line(
+                X=np.array([step]),
+                Y=np.array([reward]),
+                win=title,
+                name=data,
+                opts=dict(
+                    title=title
+                )
             )
-        )
-    elif new:
-        win = viz.line(
-            X=np.array([step]),
-            Y=np.array([reward]),
-            win=title,
-            name=data,
-            update='new',
-            opts=dict(
-                title=title
+        else:
+            win = viz.line(
+                X=np.array([step]),
+                Y=np.array([reward]),
+                win=title,
+                name=data,
+                update='new',
+                opts=dict(
+                    title=title
+                )
             )
-        )
     else:
         win = viz.line(
             X=np.array([step]),
